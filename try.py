fb3132f967a964062c5244c722083a72d66c75c4
wula50 <wula50@163.com>
2017-12-19 18:03:18 +0800

    进一步说明input和raw_input的区别

--- try.py
+++ try.py
@@ -18,3 +18,20 @@ name=raw_input()  #接受字符串
 print(name)
 n=input()   #接受合法的表达式
 print(n)
+
+'''错误
+a
+a
+a
+Traceback (most recent call last):
+  File "C:/Users/air/PycharmProjects/LearningPython/venv/demofirst.py", line 3, in <module>
+    n=input()
+  File "<string>", line 1, in <module>
+NameError: name 'a' is not defined
+
+正确
+a
+a
+'a'
+a
+'''
