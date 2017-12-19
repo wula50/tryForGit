2bccf6d1cc5d2d7d87c6a8b7d87bd62a96c255a8
wula50 <wula50@163.com>
2017-12-19 11:41:45 +0800

    课后试一试1

---
+++ venv/haveAtry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#一周有多少分钟
+s=7*24*60
+print(s)
+
+#编写小程序，打印3行：包括名字，出生日期，喜欢的颜色
+print("My name is Ada Zheng.\n I was born May 24, 2000.\n My favorite color is Red.")
