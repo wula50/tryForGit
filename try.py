362fde2750cc5d0748e848bdeda8689956ae4cd8
wula50 <wula50@163.com>
2017-12-19 10:15:05 +0800

    添加了一起学编程中的1-1

--- try.py
+++ try.py
@@ -1,1 +1,11 @@
 print("hello github")
+
+'''if timsAnswer==correctAnswer:
+    print("You got it right!")
+    score=score+10
+'''
+
+print("I love pizza!")
+print("pizza "*20)
+print("yum "*40)
+print("I'm full.")
