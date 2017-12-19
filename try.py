4efed3778daa51c8b8db838c32d8c983fa8b9a8d
wula50 <wula50@163.com>
2017-12-19 11:01:09 +0800

    对比raw_input和input方法

--- try.py
+++ try.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 print("hello github")
 
 '''if timsAnswer==correctAnswer:
@@ -5,7 +8,13 @@ print("hello github")
     score=score+10
 '''
 
+#1-1
 print("I love pizza!")
 print("pizza "*20)
 print("yum "*40)
 print("I'm full.")
+
+name=raw_input()  #接受字符串
+print(name)
+n=input()   #接受合法的表达式
+print(n)
