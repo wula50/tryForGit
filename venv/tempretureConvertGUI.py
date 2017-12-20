38847cba9b750f2a3b4657005934d74c3b3b9b64
wula50 <wula50@163.com>
2017-12-20 12:30:13 +0800

    温度转换为GUI界面

---
+++ venv/tempretureConvertGUI.py
@@ -0,0 +1,9 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import easygui
+
+#把温度从华氏度转换成摄氏度。转换公式C=5/9*(F-32)
+f=float(easygui.enterbox("请输入需要转化的华氏度："))
+c=float(5)/9*(f-32)
+easygui.msgbox(str(f)+"华氏度对应的摄氏度是："+str(c))
