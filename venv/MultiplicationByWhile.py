ac33ee36a23bd39c126080e1b375a4c23afc3237
wula50 <wula50@163.com>
2017-12-21 11:33:15 +0800

    添加了注释

--- venv/MultiplicationByWhile.py
+++ venv/MultiplicationByWhile.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 
 '''
-乘法表 for实现
+乘法表 while实现
 '''
 print("Which multiplication table would you like?")
 number = int(raw_input())
 print("Here's your table:")
-for i in range(1, 11):
+i=1
+while i<11:
     print(str(number) + " * " + str(i) + " = " + str(number * i))
+    i+=1
