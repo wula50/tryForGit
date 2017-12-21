2fe173727cad2d7c441df23bc59072858eb552f8
wula50 <wula50@163.com>
2017-12-21 11:36:40 +0800

    乘法表，支持由用户输入乘到几

--- venv/MultiplicationByWhile.py
+++ venv/MultiplicationByWhile.py
@@ -3,11 +3,13 @@
 
 '''
 乘法表 while实现
+支持用户输入最大乘到几
 '''
 print("Which multiplication table would you like?")
 number = int(raw_input())
+i = 1
+userInput = int(raw_input("Which number do you want to mutiply?"))
 print("Here's your table:")
-i=1
-while i<11:
+while i <= userInput:
     print(str(number) + " * " + str(i) + " = " + str(number * i))
-    i+=1
+    i += 1
