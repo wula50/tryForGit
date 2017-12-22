9cf9e0270176c48662d036241547aa316293a46a
wula50 <wula50@163.com>
2017-12-22 21:50:04 +0800

    倒计时器，带*输出，我一直以为我的开发环境是3.5以上，今天输出不换行的时候发现，是2.7

--- venv/Timer.py
+++ venv/Timer.py
@@ -1,10 +1,22 @@
 # !/usr/bin/python
 # -*- coding: utf-8 -*-
 
+'''
+Countdown timer: How many seconds?4
+4 * * * *
+3 * * *
+2 * *
+1 *
+BLAST OFF!
+'''
 import time
+#import sys
 
 seconds = int(raw_input("Countdown timer: How many seconds?"))
 for i in range(seconds, 0, -1):
-    print i
+    print i,
+    for j in range(0,i):
+        print "*",  #sys.stdou.wirte("*")
     time.sleep(1)
+    print
 print("BLAST OFF!")
