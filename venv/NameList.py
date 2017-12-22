28d11567a32e129691c1c271d0e0eb227afa6742
wula50 <wula50@163.com>
2017-12-22 22:28:34 +0800

    只显示第三个输入的名字

--- venv/NameList.py
+++ venv/NameList.py
@@ -19,9 +19,11 @@ for i in range(0, 5):
 print "The names are",
 for j in nameList:
     print j, " ",
-print 
+print
 newNameList=nameList[:]
 newNameList.sort()
 print "sort nameList:",
 for k in newNameList:
     print k," ",
+print
+print "The third name you entered is:",nameList[2]
