d5ae6191e1a34fd53dcdd218fda3f917089ca9e2
wula50 <wula50@163.com>
2017-12-23 16:41:07 +0800

    使用列表作为函数参数

---
+++ venv/PrintAddress.py
@@ -0,0 +1,15 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+可以打印全世界的人名、地址、街道、城市、州或省、邮政编码和国家
+'''
+def printAddress(list=[]):
+    print(list[0] + '\n' + list[1] + ' ' + list[2] + '\n' + list[3] + ', ' + list[4] + '\n' + list[5] + ', ' + list[6])
+
+
+list = []
+for i in range(7):
+    list.append(raw_input())
+
+printAddress(list)
