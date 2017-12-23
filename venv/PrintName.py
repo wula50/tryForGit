b7e96c15655e179ee38ea3a9177db05723be2412
wula50 <wula50@163.com>
2017-12-23 15:47:15 +0800

    打印名字图

---
+++ venv/PrintName.py
@@ -0,0 +1,12 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def printName():
+    print '  A    D D     A'
+    print ' AAA   D  D   AAA'
+    print 'A   A  D D   A   A '
+
+
+for i in range(10):
+    printName()
+    print
