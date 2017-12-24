44a740ae8fd73c3bc8ba81c1418ff48593ad0cf8
wula50 <wula50@163.com>
2017-12-24 20:19:28 +0800

    格式的调整

--- venv/HotdogExample.py
+++ venv/HotdogExample.py
@@ -3,32 +3,36 @@
 
 class HotDog:
     def __init__(self):
-        self.cooked_level=0
-        self.cooked_string="Raw"
-        self.condiments=[]
+        self.cooked_level = 0
+        self.cooked_string = "Raw"
+        self.condiments = []
+
     def __str__(self):
-        msg="hot dog"
-        if len(self.condiments)>0:
-            msg=msg+" with "
+        msg = "hot dog"
+        if len(self.condiments) > 0:
+            msg = msg + " with "
         for i in self.condiments:
-            msg=msg+i+", "
-        msg=msg.strip(",")
-        msg=self.cooked_string+" "+msg+"."
+            msg = msg + i + ", "
+        msg = msg.strip(",")
+        msg = self.cooked_string + " " + msg + "."
         return msg
-    def cook(self,time):
-        self.cooked_level=self.cooked_level+time
-        if self.cooked_level>8:
-            self.cooked_string="Charecoal"
-        elif self.cooked_level>5:
-            self.cooked_string="Well-done"
-        elif self.cooked_level>3:
-            self.cooked_string="Medium"
+
+    def cook(self, time):
+        self.cooked_level = self.cooked_level + time
+        if self.cooked_level > 8:
+            self.cooked_string = "Charecoal"
+        elif self.cooked_level > 5:
+            self.cooked_string = "Well-done"
+        elif self.cooked_level > 3:
+            self.cooked_string = "Medium"
         else:
-            self.cooked_string="Raw"
-    def addCondiment(self,condiment):
+            self.cooked_string = "Raw"
+
+    def addCondiment(self, condiment):
         self.condiments.append(condiment)
 
-myDog=HotDog()
+
+myDog = HotDog()
 print myDog
 print "Cooking hot dog for 4 minutes..."
 myDog.cook(4)
