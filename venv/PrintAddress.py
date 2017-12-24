27dd7d052b959d63d3eb0caf2319494499599dbd
wula50 <wula50@163.com>
2017-12-24 20:19:58 +0800

    打印地址信息

--- venv/PrintAddress.py
+++ venv/PrintAddress.py
@@ -4,6 +4,8 @@
 '''
 可以打印全世界的人名、地址、街道、城市、州或省、邮政编码和国家
 '''
+
+
 def printAddress(list=[]):
     print(list[0] + '\n' + list[1] + ' ' + list[2] + '\n' + list[3] + ', ' + list[4] + '\n' + list[5] + ', ' + list[6])
 
