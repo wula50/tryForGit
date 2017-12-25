aaaa0dacf4990a5a88821d0a2ba2279c8b661b7d
wula50 <wula50@163.com>
2017-12-25 17:07:24 +0800

    引入模块的方法

---
+++ venv/my_Model.py
@@ -0,0 +1,7 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def printName():
+    print '  A    D D     A'
+    print ' AAA   D  D   AAA'
+    print 'A   A  D D   A   A '
---
+++ venv/test.py
@@ -0,0 +1,12 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+引入模块的两种方法
+'''
+# import my_Model
+# my_Model.printName()
+
+
+from my_Model import printName
+
+printName()
