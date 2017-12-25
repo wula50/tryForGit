7d2d5fc6f84e6ec9118bb9553b3dbc11019a84b2
wula50 <wula50@163.com>
2017-12-25 20:32:13 +0800

    随机画各种颜色的长方形

--- venv/16-1.py
+++ venv/16-1.py
@@ -2,13 +2,23 @@
 # -*- coding: utf-8 -*-
 '''
 画一个绿色的圆环
+
+画100个长方形
 '''
-import pygame, sys
+import pygame, sys, random
 
 pygame.init()
 screen = pygame.display.set_mode([640, 480])
-screen.fill([255,255,255])
-pygame.draw.circle(screen,[0,255,0],[100,100],100,50)
+screen.fill([255, 255, 255])
+# pygame.draw.circle(screen,[0,255,0],[100,100],100,50)
+for i in range(50):
+    top = random.randint(0, 250)
+    left = random.randint(0, 400)
+    wide = random.randint(0, 480)
+    length = random.randint(0, 300)
+    linewide = random.randint(0, 10)
+    colorname = random.choice(pygame.color.THECOLORS.keys())
+    pygame.draw.rect(screen, pygame.color.THECOLORS[colorname], [top, length, wide, length], 2)
 pygame.display.flip()
 while True:
     for event in pygame.event.get():
