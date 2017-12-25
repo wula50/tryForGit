5a079074ec11ab8972b61f45872c44091ece115e
wula50 <wula50@163.com>
2017-12-25 22:57:24 +0800

    波峰的问题没有解决

--- venv/sinX.py
+++ venv/sinX.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 '''
 尝试画正弦曲线
+
+波峰不够高？
 '''
 import pygame, sys
 import math
@@ -11,11 +13,11 @@ screean = pygame.display.set_mode([640, 480])
 
 screean.fill([255, 255, 255])
 for x in range(640):
-    pygame.draw.rect(screean,[0,0,0],[x,100,1,1],1)
+    pygame.draw.rect(screean, [0, 0, 0], [x, 100, 1, 1], 1)
 for y in range(640):
-    pygame.draw.rect(screean,[0,0,0],[100,y,1,1],1)
+    pygame.draw.rect(screean, [0, 0, 0], [100, y, 1, 1], 1)
 for y in range(0, 10000):
-    x =(math.sin(200*y))/1000000+200
+    x = (math.sin(200 * y)) / 1000000 + 200
     pygame.draw.rect(screean, [0, 0, 0], [x, y, 1, 1], 1)
 pygame.display.flip()
 while True:
