7091cc1a361c14fdc3c793e12a14e2cd88c96437
wula50 <wula50@163.com>
2017-12-26 13:06:02 +0800

    小球满屏动

--- venv/ball.py
+++ venv/ball.py
@@ -7,6 +7,8 @@
 隐藏第一个球
 
 小球来回动
+
+小球满屏动
 '''
 import pygame, sys
 
@@ -17,18 +19,22 @@ my_ball = pygame.image.load("ball.png")
 x = 50
 y = 50
 x_speed = 20
-screen.blit(my_ball, [50, 50])
-pygame.display.flip()
+y_speed = 20
+# screen.blit(my_ball, [50, 50])
+# pygame.display.flip()
 # for looper in range(1, 5):#使小球一直动
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    pygame.time.delay(10)
+    pygame.time.delay(20)
     pygame.draw.rect(screen, [255, 255, 255], [x, y, 255, 255], 0)
     x += x_speed
+    y += y_speed
     if x > screen.get_width() - 255 or x < 0:
         x_speed = -x_speed
-    screen.blit(my_ball, [x, 50])
+    if y > screen.get_height() - 255 or y < 0:
+        y_speed = -y_speed
+    screen.blit(my_ball, [x, y])
     pygame.display.flip()
