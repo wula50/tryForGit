cc5ac2ea6d02c833389f10a2059420cbe8c962be
wula50 <wula50@163.com>
2017-12-26 12:53:08 +0800

    小球来回水平动

--- venv/ball.py
+++ venv/ball.py
@@ -5,20 +5,30 @@
 显示2个球
 
 隐藏第一个球
+
+小球来回动
 '''
 import pygame, sys
 
 pygame.init()
-screen = pygame.display.set_mode([640, 480])
+screen = pygame.display.set_mode([800, 480])
 screen.fill([255, 255, 255])
 my_ball = pygame.image.load("ball.png")
+x = 50
+y = 50
+x_speed = 20
 screen.blit(my_ball, [50, 50])
 pygame.display.flip()
-pygame.time.delay(2000)
-screen.blit(my_ball, [300, 50])
-pygame.draw.rect(screen,[255,255,255],[50,50,255,255],0)
-pygame.display.flip()
+# for looper in range(1, 5):#使小球一直动
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+    pygame.time.delay(10)
+    pygame.draw.rect(screen, [255, 255, 255], [x, y, 255, 255], 0)
+    x += x_speed
+    if x > screen.get_width() - 255 or x < 0:
+        x_speed = -x_speed
+    screen.blit(my_ball, [x, 50])
+    pygame.display.flip()
