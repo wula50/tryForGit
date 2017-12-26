fbec1bc84fed411592fed3a59ba6a7a65e889366
wula50 <wula50@163.com>
2017-12-26 12:33:53 +0800

    隐藏第一个球

--- venv/ball.py
+++ venv/ball.py
@@ -3,6 +3,8 @@
 
 '''
 显示2个球
+
+隐藏第一个球
 '''
 import pygame, sys
 
@@ -13,7 +15,8 @@ my_ball = pygame.image.load("ball.png")
 screen.blit(my_ball, [50, 50])
 pygame.display.flip()
 pygame.time.delay(2000)
-screen.blit(my_ball, [150, 50])
+screen.blit(my_ball, [300, 50])
+pygame.draw.rect(screen,[255,255,255],[50,50,255,255],0)
 pygame.display.flip()
 while True:
     for event in pygame.event.get():
